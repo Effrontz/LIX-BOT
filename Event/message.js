fa2e0e771fdd3a740e57435520d4c7121c3359d3
Legenda <84774591+legenda0520@users.noreply.github.com>
2021-08-01 21:53:27 +0200

    Add files via upload

---
+++ Event/message.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const prefix = ""
+
+module.exports = async(client, message) => {
+
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return console.log(`Message provenant des mp: '${message}' qui provient de '${message.author.username}'`), message.reply('**Merci de ne pas envoyer de message privée, si vous avez une question contacter: ``` Azalee.#9729 ```**')
+
+    if (!message.content.startsWith(prefix)) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const commande = args.shift();
+
+    const cmd = client.commands.get(commande);
+
+    if (!cmd) return;
+
+    cmd.run(client, message, args)}; 
---
+++ Event/ready.js
@@ -0,0 +1,5 @@
+module.exports = async(client) => {
+
+    client.user.setActivity(`Découvrez notre map fortnite créatif, dés maintennant !`,{ type: 'WATCHING' });
+
+}
